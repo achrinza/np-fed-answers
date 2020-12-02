fd188872716c946335a1bacaa67c68b66f117aca
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2020-12-02 10:39:52 +0800

    feat(DSA): add week 4 assignment
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

---
+++ DSA/Assignments/Week 4/WebHistoryApp/Stack.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2020 Rifa I. Achrinza
+// This document is licensed under MIT license (See LICENSE for details)
+// SPDX-Short-Identifier: MIT
+
+#pragma once
+#include <iostream>
+
+typedef std::string ItemType;
+
+class Stack
+{
+private:
+	struct Node
+	{
+		ItemType item;   // to store data
+		Node* next;  // to point to next node
+	};
+
+	Node* topNode{ NULL };
+
+public:
+	Stack();		// constructor
+	~Stack();		// destructor
+	bool push(ItemType item);
+	bool pop();
+	bool pop(ItemType& item);
+	void getTop(ItemType& item);
+	bool isEmpty();
+	void displayInOrder();
+	void displayInOrderOfInsertion();
+};
